bc96524f82238bd7bc4de0dcb073759cc13e1bc6
ardigermenji <ardi.germenji@gmail.com>
2021-04-06 11:22:19 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-  return <h1>Hello Build Week!</h1>;
-};
-
-export default App;
+export default class App extends Component {
+  componentDidMount = async () => {
+    const resp = await fetch(
+      'https://striveschool-api.herokuapp.com/api/profile/5fc4ae95b708c200175de88d',
+      {
+        headers: {
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDZjMWEyNzZmZDIyODAwMTUzZmRiYjEiLCJpYXQiOjE2MTc2OTczMTksImV4cCI6MTYxODkwNjkxOX0.bSzAALu5Ose7Gdie6QifObaHxeHflzff7nHtUlrYWfI',
+        },
+      }
+    );
+    const data = await resp.json();
+    console.log(data);
+  };
+  render() {
+    return <div></div>;
+  }
+}
