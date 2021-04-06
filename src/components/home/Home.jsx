c1fff5e8ccf3438fed7e6fafd551d20bcba68984
ardigermenji <ardi.germenji@gmail.com>
2021-04-06 12:32:41 +0200

    fetch user data in home component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,11 +4,31 @@ import Main from './main-component/Main';
 import Side from './side-col/Side';
 
 export default class Home extends Component {
+  state = {
+    user: {},
+  };
+
+  componentDidMount = async () => {
+    const resp = await fetch(
+      'https://striveschool-api.herokuapp.com/api/profile/me',
+      {
+        headers: {
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDZjMWEyNzZmZDIyODAwMTUzZmRiYjEiLCJpYXQiOjE2MTc2OTczMTksImV4cCI6MTYxODkwNjkxOX0.bSzAALu5Ose7Gdie6QifObaHxeHflzff7nHtUlrYWfI',
+        },
+      }
+    );
+    const data = await resp.json();
+    console.log(data);
+
+    this.setState({ user: data });
+  };
+
   render() {
     return (
       <Container>
         <Row>
-          <Main />
+          <Main user={this.state.user} />
           <Side />
         </Row>
       </Container>
