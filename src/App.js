7005932ea1e160b0bc51ac16da31d0ccb8e036f1
Jamie <jamiekyaellis@gmail.com>
2021-04-06 12:59:42 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Home from './components/home/Home';
+import React, { Component } from "react";
+import Home from "./components/home/Home";
 
 export default class App extends Component {
   render() {
