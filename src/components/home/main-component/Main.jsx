a74ced9c2428a6db3e30db1bc199875ebc69fda9
ardigermenji <ardi.germenji@gmail.com>
2021-04-06 12:17:11 +0200

    adds main component

--- src/components/home/main-component/Main.jsx
+++ src/components/home/main-component/Main.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 const Main = () => {
-  return <div>ciao</div>;
+  return (
+    <Col md={9}>
+      <Row className='flex-column'>
+        <h1>Main Component</h1>
+      </Row>
+    </Col>
+  );
 };
 
 export default Main;
