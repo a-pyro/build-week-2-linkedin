0062404cd4a3ac3e7df898727359236ccde19db9
Jamie <jamiekyaellis@gmail.com>
2021-04-06 12:43:23 +0200

    up

--- src/components/home/main-component/Main.jsx
+++ src/components/home/main-component/Main.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
-import { Container, Row, Col} from 'react-bootstrap'
-import {} from 'react-router-dom'
-import PersonalDetails from './PersonalDetails'
+import React from 'react';
+import { Col } from 'react-bootstrap';
+import PersonalDetails from './PersonalDetails';
+const Main = () => {
+  return (
+    <Col style={mainStyle} md={8}>
+      <h1>Main Component</h1>
+    </Col>
+  );
+};
 
- 
+export default Main;
+
+const mainStyle = {
+  background: '#7a7af0',
+};
