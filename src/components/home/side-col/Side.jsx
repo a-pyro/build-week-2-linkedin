0df83668f966ebb940f1570923e35a5838d33a21
ardigermenji <ardi.germenji@gmail.com>
2021-04-07 20:06:58 +0200

    adds edit pubblic prof in sidebar

--- src/components/home/side-col/Side.jsx
+++ src/components/home/side-col/Side.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
+import EditPublicProfile from './EditPublicProfile';
 
 const Side = () => {
   return (
     <Col md={4}>
-      <h1>side Component</h1>
-      <h1>side Component</h1>
-      <h1>side Component</h1>
-      <h1>side Component</h1>
-      <h1>side Component</h1>
-      <h1>side Component</h1>
-      <h1>side Component</h1>
-      <h1>side Component</h1>
-      <h1>side Component</h1>
-      <h1>side Component</h1>
-      <h1>side Component</h1>
-      <h1>side Component</h1>
+      <EditPublicProfile />
     </Col>
   );
 };
