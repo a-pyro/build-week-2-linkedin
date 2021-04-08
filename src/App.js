3d5a3a365e831fdf121dc9d5c9010e0031073abc
Ekow <ewusiessel@gmail.com>
2021-04-08 11:13:51 +0100

    Revert "removed language flags"
    This reverts commit 05dd43b27fcc2c03b9fce5308afe8a94afba3136.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
-import Home from "./components/home/Home";
 import Footer from "./components/Footer";
 
 export default class App extends Component {
+  // componentDidMount = async () => {
+  //   const resp = await fetch(
+  //     'https://striveschool-api.herokuapp.com/api/profile/5fc4ae95b708c200175de88d',
+  //     {
+  //       headers: {
+  //         Authorization:
+  //           'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDZjMWEyNzZmZDIyODAwMTUzZmRiYjEiLCJpYXQiOjE2MTc2OTczMTksImV4cCI6MTYxODkwNjkxOX0.bSzAALu5Ose7Gdie6QifObaHxeHflzff7nHtUlrYWfI',
+  //       },
+  //     }
+  //   );
+  //   const data = await resp.json();
+  //   console.log(data);
+  // };
   render() {
     return (
       <div>
-        <Home />
         <Footer />
       </div>
     );
