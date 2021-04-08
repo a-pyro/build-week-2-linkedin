05dd43b27fcc2c03b9fce5308afe8a94afba3136
Ekow <ewusiessel@gmail.com>
2021-04-08 10:18:45 +0100

    removed language flags

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import { Container, Row } from "react-bootstrap";
+import Main from "./main-component/Main";
+import Side from "./side-col/Side";
 
-const Footer = function () {
-  return <div className="container">hello</div>;
-};
+export default class Home extends Component {
+  state = {
+    user: {},
+  };
 
-export default Footer;
+  componentDidMount = async () => {
+    const resp = await fetch(
+      "https://striveschool-api.herokuapp.com/api/profile/me",
+      {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDZjMWEyNzZmZDIyODAwMTUzZmRiYjEiLCJpYXQiOjE2MTc2OTczMTksImV4cCI6MTYxODkwNjkxOX0.bSzAALu5Ose7Gdie6QifObaHxeHflzff7nHtUlrYWfI",
+        },
+      }
+    );
+    const data = await resp.json();
+    console.log(data);
+
+    this.setState({ user: data });
+  };
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Main user={this.state.user} />
+          <Side />
+        </Row>
+      </Container>
+    );
+  }
+}
---
+++ src/components/home/main-component/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+
+const Main = () => {
+  return (
+    <Col style={mainStyle} md={8}>
+      <h1>Main Component</h1>
+    </Col>
+  );
+};
+
+export default Main;
+
+const mainStyle = {
+  background: '#7a7af0',
+};
