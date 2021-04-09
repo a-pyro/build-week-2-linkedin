e5e00d2694f4c763837a81d2def4518b72d19306
Jamie <jamiekyaellis@gmail.com>
2021-04-09 11:51:56 +0200

    up

--- src/components/home/main-component/ProfileDetails.jsx
+++ src/components/home/main-component/ProfileDetails.jsx
@@ -47,35 +47,42 @@ const FlexColRow = styled(Col)`
 const FlexColColumn = styled(Col)`
   display: flex;
   flex-direction: column;
-  
 `;
 class PersonalDetails extends React.Component {
   state = {
-    user: {
-      area: "",
-      bio: "",
-      email: "",
-      image: "",
-      name: "",
-      surname: "",
-      title: "",
-      username: "",
-    },
+    user: {},
   };
 
+  replaceBrokenImg = (e) => {
+    console.log("img src not fount, dont worrie, got a fallback :)");
+    e.target.src = `https://picsum.photos/100/100?random=${Math.ceil(
+      Math.random() * 1000
+    )}`;
+  };
 
-  fetchUser = async () =>  {
-      let response = await fetch("https://striveschool-api.herokuapp.com/api/profile/ ", {
-          method: "GET",
-          headers: {
-            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDcwMTQzODUxZmFhYTAwMTViYWRmZGYiLCJpYXQiOjE2MTc5NTc5NDUsImV4cCI6MTYxOTE2NzU0NX0.yjLY9UryAgaHA9UblurFwQBEiZJJFT4DIsYWIsZ6KQE"
-          },
-      })
-      let data = response.json()
+  fetchUser = async () => {
+    let response = await fetch(
+      "https://striveschool-api.herokuapp.com/api/profile/me ",
+      {
+        method: "GET",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDcwMTQzODUxZmFhYTAwMTViYWRmZGYiLCJpYXQiOjE2MTc5NTc5NDUsImV4cCI6MTYxOTE2NzU0NX0.yjLY9UryAgaHA9UblurFwQBEiZJJFT4DIsYWIsZ6KQE",
+        },
+      }
+    );
+    let data = response.json();
+    data.
+    this.setState({ user: data });
+    
+  };
+  
+  componentDidUpdate(){
+      console.log(this.state.user)
   }
-  //componentDidMount = async () =>{
-  //let response = await fetch ()
-  //}
+  componentDidMount() {
+    this.fetchUser();
+  }
   render() {
     return (
       <StyledContainer>
@@ -85,24 +92,37 @@ class PersonalDetails extends React.Component {
         <DetailsDiv>
           <Row>
             <FlexColRow className="col-12">
-              <ProfileImage src="https://media-exp1.licdn.com/dms/image/C4D03AQHrd2-LuPm_dA/profile-displayphoto-shrink_200_200/0/1612284608867?e=1623283200&v=beta&t=FbTmMKnbtrexZQFzmWfEi_e3WSWzQ0xEcavfkrxHJsQ" />
+              <ProfileImage
+                onError={this.replaceBrokenImg}
+                src={this.state.user.image}
+              />
               <BsPencil className="mt-2" />
             </FlexColRow>
           </Row>
           <Row>
             <FlexColRow className="col-7 mt-3">
-              <h4>Jamie Ellis</h4>
+              <h4>
+                {this.state.user.name} {this.state.user.surname}
+              </h4>
             </FlexColRow>
-            <Col style={{display: "flex"}}className="col-5 mt-3">
-              <img alt="education" width="32px" height="32px" src="https://media-exp1.licdn.com/dms/image/C4D0BAQFFQIjyDsOK0w/company-logo_100_100/0/1593351903670?e=1625702400&v=beta&t=3rWOTtMt8Oa6T_gmK3KDVW1m0AsgXMfL-JXwC0n4tXo"/>
-              <h6 style={{margin: "6px 0 0 2px"}}>Strive School</h6>
+            <Col style={{ display: "flex" }} className="col-5 mt-3">
+              <img
+                alt="education"
+                width="32px"
+                height="32px"
+                src="https://media-exp1.licdn.com/dms/image/C4D0BAQFFQIjyDsOK0w/company-logo_100_100/0/1593351903670?e=1625702400&v=beta&t=3rWOTtMt8Oa6T_gmK3KDVW1m0AsgXMfL-JXwC0n4tXo"
+              />
+              <h6 style={{ margin: "6px 0 0 2px" }}>Strive School</h6>
             </Col>
           </Row>
           <Row>
-          <FlexColColumn>
-            <h6>Full-Stack Software Engineering at Strive School</h6>
-            <h6>Marbella, Andalusia, Spain -<a href="">56 connections</a>  -<a href="">Contact info</a></h6>  
-          </FlexColColumn>
+            <FlexColColumn>
+              <h6>Full-Stack Software Engineering at Strive School</h6>
+              <h6>
+                {this.state.user.area} -<a href="">56 connections</a> -
+                <a href="">Contact info</a>
+              </h6>
+            </FlexColColumn>
           </Row>
           <Row>
             <Col className="col-12 mt-3">
@@ -110,9 +130,6 @@ class PersonalDetails extends React.Component {
               <Button>Add profile section</Button>
               <Button>More...</Button>
             </Col>
-          </Row>
-          <Row>
-            <span>{}</span>
           </Row>
         </DetailsDiv>
       </StyledContainer>
