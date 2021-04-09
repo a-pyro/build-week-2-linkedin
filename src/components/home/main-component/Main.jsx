591cad939e722c01ad31e4bd978cd5950fc7462c
Jamie <jamiekyaellis@gmail.com>
2021-04-09 10:50:33 +0200

    up

--- src/components/home/main-component/Main.jsx
+++ src/components/home/main-component/Main.jsx
@@ -6,13 +6,17 @@ import Dashboard from './Dashboard';
 import Activity from './Activity';
 import Interests from './Interests';
 import Skills from './Skills';
+import Education from'./Education';
+import PersonalDetails from './ProfileDetails';
 // import Dashboard from 'components/home/main-component/Dashboard';
 
 const Main = () => {
   return (
     <Col md={8}>
+      <PersonalDetails />
       <ProfileStrength />
       <Dashboard />
+      <Education />
       <Activity />
       <Interests />
       <Skills />
