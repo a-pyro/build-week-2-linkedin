9cb9928dd1279429c4b093aa385b19e1519b3d21
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-05-12 16:10:12 +0200

    adds login component

--- src/components/Pages/registration/Login.jsx
+++ src/components/Pages/registration/Login.jsx
@@ -1,11 +1,98 @@
 import React, { Component } from 'react';
+import { Col, Container, Row, Form, Button, Spinner } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom';
 
-export default class Login extends Component {
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    loading: false,
+    isError: false,
+    errorMessage: '',
+  };
+
+  handleChange = (e) => {
+    const value = e.target.value;
+    const target = e.target.name;
+    this.setState({ ...this.state, [target]: value });
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      this.setState({ loading: true });
+      const resp = await fetch(`${process.env.REACT_APP_API_URL}/auth/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: this.state.email,
+          password: this.state.password,
+        }),
+      });
+      const data = await resp.json();
+      if (resp.ok) {
+        localStorage.setItem('token', data.token);
+        this.props.history.push('/newsfeed');
+      } else {
+        console.log(data);
+        this.setState({ isError: true, errorMessage: data.error });
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
   render() {
     return (
-      <div>
-        <h1>Login</h1>
-      </div>
+      <Container>
+        <Row className='justify-content-center align-items-center min-vh-100'>
+          <Col className='' xs={6}>
+            <h1>Login</h1>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group controlId='formBasicEmail'>
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type='email'
+                  name='email'
+                  placeholder='Enter email'
+                  onChange={this.handleChange}
+                />
+                <Form.Text className='text-muted'>
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId='formBasicPassword'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type='password'
+                  name='password'
+                  placeholder='Password'
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              {this.state.loading && (
+                <div className='text-center'>
+                  <Spinner animation='grow' />
+                </div>
+              )}
+              {this.state.isError && (
+                <p className='text-danger'>{this.state.errorMessage}</p>
+              )}
+
+              <Button variant='primary' type='submit'>
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
+
+export default withRouter(Login);
