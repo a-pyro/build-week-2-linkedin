17eacfc62d81ee4e815d2f82f81b3bdb669dbe09
Flynn Stacey <flynnstacey@hotmail.com>
2021-05-14 16:09:06 +0100

    added comments to posts

--- src/components/news-feed-comp/PostList.jsx
+++ src/components/news-feed-comp/PostList.jsx
@@ -1,19 +1,19 @@
-import SinglePost from './SinglePost';
-import { v4 as uuidv4 } from 'uuid';
-import { Row, Spinner } from 'react-bootstrap';
+import SinglePost from "./SinglePost";
+import { v4 as uuidv4 } from "uuid";
+import { Row, Spinner } from "react-bootstrap";
 
 const PostList = ({ posts, userLogged, getPosts, isLoading }) => {
   // console.log(userLogged);
   // console.log(posts);
   if (isLoading)
     return (
-      <Row className='justify-content-center mt-3'>
-        <Spinner animation='grow' />
+      <Row className="justify-content-center mt-3">
+        <Spinner animation="grow" />
       </Row>
     );
   console.log(posts);
   return (
-    <Row className='mt-3'>
+    <Row className="mt-3">
       {posts.map((post) => (
         <SinglePost
           getPosts={getPosts}
